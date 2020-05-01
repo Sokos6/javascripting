554b4221c20f5eed210f3693ff3829ad25593b9b
Will Sokolowski <will.sokolowski@gmail.com>
2020-05-01 13:22:43 -0500

    character input handler

---
+++ charactersupport.js
@@ -0,0 +1,41 @@
+// input element created on index.js for demo
+
+// Handle Events -
+// By handling the keypress event, we can prevent users from entering characters
+// in this case, digits and space
+
+const ele = document.getElementById('input');
+
+ele.addEventListener('keypress', function(e) {
+  // Get the code of pressed key
+  const key = e.which || e.keyCode;
+
+  if (key != 32 && (key < 48 || key > 57)) {
+    e.preventDefault();
+  }
+});
+
+let currentValue = ele.value || '';
+
+ele.addEventListenerI('input', function(e) {
+  const target = e.target;
+
+  // If users enter supported character (digits or space)
+  /^[0-9\s]*$/.test(target.value)
+  // Backup the current value
+  ? currentValue = target.value
+  // Otherwise, restore the value
+  // Note that in this case, `e.preventDefault()` doesn't help
+  : target.value = currentValue;
+});
+
+const selection = {};
+
+ele.addEventListener('keydown', function(e) {
+  const target = e.target;
+  selection = {
+    selectionStart: target.selectStart,
+    selectionEnd: target.selectionEnd,
+  };
+});
+
