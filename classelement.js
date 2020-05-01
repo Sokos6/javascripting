ccc3e4b10a49a8d49e5a4210c75c827f60c50639
Will Sokolowski <will.sokolowski@gmail.com>
2020-05-01 12:53:38 -0500

    class add remove toggle

---
+++ classelement.js
@@ -0,0 +1,16 @@
+// Add a class to an element
+
+ele.classList.add('class-name');
+
+// add muiltiple classes - not supported by IE 11
+
+ele.classList.add('another', 'class', 'name');
+
+// Remove a class from an element
+
+ele.classList.remove('class-name');
+
+ele.classList.remove('another', 'class', 'name');
+
+// Toggle a class
+ele.classList.toggle('class-name');
