174050b8405237d1b2627d01de00cfbdf3edd398
Will Sokolowski <will.sokolowski@gmail.com>
2020-07-21 10:28:34 -0500

    code frequency basic lessons

---
+++ basiclesson.js
@@ -0,0 +1,12 @@
+// Coding challenge #1: Print numbers from 1 to 10
+
+for(let i = 1; i <= 10; i++) {
+  console.log(i);
+}
+
+// Coding challenge #2: Print the odd numbers less than 100
+
+
+for(let i = 1; i  <= 100; i += 2) {
+  console.log(i)
+}
--- prime.js
+++ prime.js
@@ -1,16 +1,14 @@
 function isPrime(num) {
-  if (num === 2) return true;
   if (num <= 1) return false;
-
-  let num2 = Math.sqrt(num);
-
-  for (let i = 2; i <= num2; i++) {
-    if (num % i === 0) {
-      return false;
+  if (num=== 2) return true;
+  let num2 = Math.sqrt(num);//num2 is the square root of num
+  for (let i= 2; i <= num2; i++) { //note that we are working now with the square root
+   if (num2 % i === 0) {
+   return false;
     }
   }
-  return true;
-}
-
-console.log(isPrime(40));
-console.log(isPrime(101));
+   return true;
+  }
+ isPrime(521) //is True
+ isPrime(9801) //is False
+ isPrime(13037) //is True
