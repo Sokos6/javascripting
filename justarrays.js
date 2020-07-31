931eb022202a626b448fa478368be5588051fad1
Sokos6 <will.sokolowski@gmail.com>
2020-07-31 10:41:49 -0500

    updated array work

--- justarrays.js
+++ justarrays.js
@@ -15,13 +15,13 @@ const colors = ['red', 'green', 'blue', 'orange', 'yellow', 'purple'];
 
 console.log(colors.length);
 
- console.log(colors[colors.length - 1]);
+console.log(colors[colors.length - 1]);
 
- colors.length = 8;
- console.log(colors);
+colors.length = 8;
+console.log(colors);
 
- colors.length = 3;
- console.log(colors);
+colors.length = 3;
+console.log(colors);
 
 //  Pop, Push, Shift, and Unshift
 colors.pop();
@@ -29,3 +29,53 @@ console.log(colors);
 
 colors.shift();
 console.log(colors);
+
+colors.push('White');
+console.log(colors);
+
+colors.unshift('Teal');
+console.log(colors);
+
+// Merging Arrays
+// colors.concat(['Pink', 'Midnight Blue', 'Aqua']);
+console.log(colors.concat(['Pink', 'Midnight Blue', 'Aqua']));
+
+// spread operator
+NewColors = [...colors, ...['red', 'orange']];
+console.log(NewColors);
+
+console.log(colors.join(' & '));
+
+
+
+console.log(colors.slice(2, 4));
+
+// non destructive operation. Colors still intact
+console.log(colors);
+
+// The first number in the parentheses tells us the index at which to start the splice
+// The second number tells us how many items to remove from the array.
+// Notice the splice() method returns the items removed from the array as a subarray.
+
+justthis = colors.splice(1, 1, 'Gold')
+console.log(colors);
+console.log(justthis);
+
+console.log(colors.reverse());
+
+// It is alphabetical order by default for String objects. Note that this also changes the order of the array permanently.
+console.log(colors.sort());
+
+console.log([5, 9, 7].sort());
+
+
+// Finding if a Value is in an Array
+
+console.log(colors.indexOf('Teal'));
+
+console.log(colors.includes('Teal'));
+console.log(colors.includes('Black'));
+
+// You can also add an extra parameter to indicate which index to start the search from:
+console.log('Teal', 1);
+
