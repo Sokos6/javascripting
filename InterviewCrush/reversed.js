cfd4aa922546b258f1619a1de8ddd0c73f470338
Sokos6 <will.sokolowski@gmail.com>
2020-09-11 12:28:17 -0500

    array reversal through swapping

--- InterviewCrush/reversed.js
+++ InterviewCrush/reversed.js
@@ -4,3 +4,27 @@ function reversed(arr) {
 }
 
 console.log(reversed([2, 3, 4, 5, 3, 3]));
+
+// Manipulate strings in place
+// Modify the input, need a mutable type, like array
+// not an immutable string
+
+// in place algorithm swapping elements
+
+function reverse(arrayOfChars) {
+  let leftIndex = 0;
+  let rightIndex = arrayOfChars.length - 1;
+
+  while (leftIndex < rightIndex) {
+    // Swap characters
+    const temp = arrayOfChars[leftIndex];
+    arrayOfChars[leftIndex] = arrayOfChars[rightIndex];
+    arrayOfChars[rightIndex] = temp;
+
+    // Move towards middle
+    leftIndex++;
+    rightIndex--;
+  }
+}
+
+console.log(reverse(['a', 'c', 'g', 'd', 'y']));
